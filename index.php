0bae30394672c353b04dc46a189d219fab3596ef
mrkickling <loxdalen@gmail.com>
2013-11-28 09:03:31 +0100

    Created the index page
    It's nice

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+include "pref.php";
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Jordnötter</title>
+</head>
+<body>
+
+<?php include "menu.php";?>
+
+<table border='1px' width='50%'>
+<tr>
+	<th>Kategori</th>
+	<th>Skippy Peanut butter</th>
+	<th>Green choice jordnötssmör</th>
+</tr>
+
+<tr>
+	<td>Hej</td>
+	<td>Hej</td>
+	<td>Hej</td>
+</tr>
+
+</table>
+
+</body>
+</html>
